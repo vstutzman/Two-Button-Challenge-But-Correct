10fa392078f3dd7e1128fd8299070621748d848f
Victoria Stutzman <vstutzman@zagmail.gonzaga.edu>
2020-09-03 16:36:34 -0700

    Initial Commit

---
+++ Two Button Challenge But Correct/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two Button Challenge But Correct
+//
+//  Created by Victoria Stutzman on 9/3/20.
+//  Copyright © 2020 Victoria Stutzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
